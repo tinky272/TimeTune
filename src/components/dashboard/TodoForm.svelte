007ef2ac1bc72ef0a830aa0f0b37e976631306a8
tinky272 <116848094+tinky272@users.noreply.github.com>
2022-12-24 20:04:17 -0800

    Created Project but having issues with adding the items

---
+++ src/components/dashboard/TodoForm.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let todoInput;
+    export let add;
+</script>
+
+      <div>
+    
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Welcome to your Todo list</h2>
+
+      </div>
+      <form class="mt-8 space-y-6" onSubmit={add}>
+        <input type="hidden" name="remember" value="true">
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="todo-input" class="sr-only">Email address</label>
+            <input bind:value={todoInput} id="todo-input" name="email" type="text" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Todo Item">
+          </div>
+        
+        </div>
+  
+       
+        <div>
+          <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+       
+            Add todo
+          </button>
+        </div>
+      </form>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import TodoForm from "../../components/dashboard/TodoForm.svelte";
+	import TodoList from "../../components/dashboard/TodoList.svelte";
+
+    // State
+    let todoInput = ''
+    let todoList =[
+        {
+            text:"Buy more milk",
+            status: true,
+        },
+        {
+            text:"Buy Ramen",
+            status: false,
+        }
+    ];
+
+    function addToList(){
+        todoList =[
+            {text: todoInput,status:true },
+            ...todoList
+        ]
+    }
+</script>
+
+<div class="h-screen">
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-16">
+<TodoForm  todoInput={todoInput} add={addToList}/>
+<TodoList todos={todoList}/>
+</div>
+</div>
+</div>
