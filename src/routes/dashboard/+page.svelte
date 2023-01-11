f5ff12b9c1f0742b6e3162e008bbcde3e4d97764
tinky272 <116848094+tinky272@users.noreply.github.com>
2023-01-10 22:14:16 -0800

    commiting everyhing

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,33 +1,37 @@
 <script>
-    import TodoForm from "../../components/dashboard/TodoForm.svelte";
-	import TodoList from "../../components/dashboard/TodoList.svelte";
+	import TodoForm from '../../components/dashboard/TodoForm.svelte';
+	import TodoList from '../../components/dashboard/TodoList.svelte';
 
-    // State
-    let todoInput = ''
-    let todoList =[
-        {
-            text:"Buy more milk",
-            status: true,
-        },
-        {
-            text:"Buy Ramen",
-            status: false,
-        }
-    ];
+	// State
+	let todoInput = '';
+	let todoList = [
+		{
+			text: 'Buy more milk',
+			status: true
+		},
+		{
+			text: 'Buy Ramen',
+			status: false
+		}
+	];
 
-    function addToList(){
-        todoList =[
-            {text: todoInput,status:true },
-            ...todoList
-        ]
-    }
+	function addToList() {
+		todoList = [{ text: todoInput, status: true }, ...todoList];
+		todoInput = '';
+	}
+
+	function removeFromList(i) {
+		todoList.splice(i, 1);
+
+		toDoList = toDoList;
+	}
 </script>
 
 <div class="h-screen">
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="w-full max-w-md space-y-16">
-<TodoForm  todoInput={todoInput} add={addToList}/>
-<TodoList todos={todoList}/>
-</div>
-</div>
+	<div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+		<div class="w-full max-w-md space-y-16">
+			<TodoForm {todoInput} add={addToList} />
+			<TodoList todos={todoList} remove={removeFromList} />
+		</div>
+	</div>
 </div>
